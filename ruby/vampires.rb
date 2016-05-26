a95da517f852447030d0fc24172ba7cd2d67ef01
Jason Pelletier <jpelletier19@me.com>
2016-05-26 14:47:57 -0400

    add 2 conditionals to monster questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,13 +4,23 @@ monsterName = gets.chomp
 
 puts "How old are you?"
 
-monsterAge = gets.chomp
+monsterAge = gets.chomp.to_i
 
-puts "Shall we order you some garlic?"
+puts "Shall we order you some garlic? y or n"
 
 monsterGarlic = gets.chomp
 
-puts "Do you wish to enroll in life insurance?"
+puts "Do you wish to enroll in life insurance? y or n"
 
 monsterLife = gets.chomp
 
+vampChecker = nil
+
+if (monsterAge < 125) && (monsterGarlic == "y")
+   vampChecker = "Probably not a vampire"
+end
+if (monsterAge > 125) && ((monsterGarlic == "n") || (monsterLife == "n"))
+	vampChecker = "Probably a vampire"
+end
+
+ puts vampChecker 
