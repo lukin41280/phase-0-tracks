14a6d5b739dddc0945071fbd700f0fb6abd2bf84
Jason Pelletier <jpelletier19@me.com>
2016-05-26 14:21:06 -0400

    complete release 0 with puts questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+puts "What is your name?"
+
+monsterName = gets.chomp
+
+puts "How old are you?"
+
+monsterAge = gets.chomp
+
+puts "Shall we order you some garlic?"
+
+monsterGarlic = gets.chomp
+
+puts "Do you wish to enroll in life insurance?"
+
+monsterLife = gets.chomp
+
