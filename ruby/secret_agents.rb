eb1ae373b22828bf9dc5140cbe8176e5bb35c27c
Jason Pelletier <jpelletier19@me.com>
2016-05-27 12:03:11 -0400

    add decrypt method

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,15 +1,9 @@
-# encrypt pseudocode
-
+# ENCRYPT PSUEDOCODE
 # define method with string parameter
-
 # define a counter variable
-
 # create while loop for each index in string
-
 # for each index, assign string index to use .next method
-
 # increase counter variable by 1
-
 # print new string to screen
 
 
@@ -23,3 +17,23 @@ def encrypt(string)
 end
 
 encrypt("abc")
+
+# DECRYPT PSUEDOCODE
+# define method with string parameter
+# define a counter variable
+# create while loop for each index in string
+# for each index, assign string index to use .next method
+# increase counter variable by 1
+# print new string to screen
+
+def decrypt(string) 
+  alpha = "abcdefghijklmnopqrstuvwxyz"
+  index = 0
+   while index < string.length
+     string[index] = alpha[string[index]]
+     index = index + 1
+   end
+   puts string
+end
+
+decrypt("def")
