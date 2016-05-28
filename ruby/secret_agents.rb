a0a7647592af36208e5c7e82abcf6873d959fddf
Jason Pelletier <jpelletier19@me.com>
2016-05-28 10:55:44 -0400

    update all code from peer challenge. add complete decrypt method and interface questions

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -6,19 +6,23 @@
 # increase counter variable by 1
 # print new string to screen
 
-
+# had to put if statement in for Z since it makes "ab"
 def encrypt(string) 
    index = 0
    while index < string.length
-     string[index] = string[index].next
+     if string[index] == "z"
+       string[index] = "a"
+     else
+        string[index] = string[index].next
+     end
      index = index + 1
    end
-   puts string
+   return string
 end
 
-encrypt("abc")
+#encrypt("xyz")
 
-# DECRYPT PSUEDOCODE
+# DECRYPT PSUEDOCODE - original
 # define method with string parameter
 # define a counter variable
 # create while loop for each index in string
@@ -26,14 +30,57 @@ encrypt("abc")
 # increase counter variable by 1
 # print new string to screen
 
-def decrypt(string) 
-  alpha = "abcdefghijklmnopqrstuvwxyz"
-  index = 0
-   while index < string.length
-     string[index] = alpha[string[index]]
-     index = index + 1
-   end
-   puts string
+#DEC PSU - second try
+# for each character in string:
+# get positoin of string entered
+# need to determine position of letter in alphabet
+# minus 1 position from determined alphabet position
+# update character in string postion
+# print to screen
+
+def charChange(char)
+   alpha = "abcdefghijklmnopqrstuvwxyz"  
+   charPosition = alpha.index(char)
+   newChar = alpha[charPosition - 1]
+   return newChar
 end
 
-decrypt("def")
+   
+def decrypt(string) 
+  index = 0
+   while index < string.length
+     movedChar = charChange(string[index])
+     string[index] = movedChar
+     index = index + 1
+   end
+   return string
+end
+
+# Test the nested methods.. will return the given input
+puts decrypt(encrypt("swordfish"))
+
+# INTERFACE QUESTIONS
+# ask user if they want to encrypt/decrypt
+# save response
+# if response is to encrypt, print a message asking user for the password
+# save the password
+# print the method using the password as a argument
+# repeat last three steps for decrypt
+# end
+
+
+puts "Agent, would you like to encrypt or decrypt a password today?"
+  response = gets.chomp
+  if response == "encrypt"
+    puts "Great, we'll encrypt. What is the password?"
+    password = gets.chomp
+    puts encrypt(password)
+  else
+    puts "Looks like we're decrypting. What's the password?"
+    password = gets.chomp
+    puts decrypt(password)
+  end
+    
+
+
+
