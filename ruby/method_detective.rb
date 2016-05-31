62584b392e160ac94f87cd722ab88d0abcd4fbf8
Jason Pelletier <jpelletier19@me.com>
2016-05-31 14:15:39 -0400

    complete first 3 methods

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,13 +4,13 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1, "o")
 # => “zoom”
 
-# "enhance".<???>
+"enhance".center(15)
 # => "    enhance    "
 
 # "Stop! You’re under arrest!".<???>
