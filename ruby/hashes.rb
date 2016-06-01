bb5e686813b47150d405d7b4e014d6a92b4ad27e
Jason Pelletier <jpelletier19@me.com>
2016-06-01 16:34:30 -0400

    add prompt questions and convert to correct type

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,21 +1,52 @@
 # DESIGNER HASH
 
 # create empty hash
+client_data = {}
 
-# propmt user for name, age, number of rooms, size of family, 
-#  favorite color scheme, decor theme, will be financing?, return client?
-#  and convert data types when necessary
+# propmt user for name, age, number of rooms, favorite color scheme, 
+	# decor theme, will be financing?, return client?
+	# and convert data types when necessary
+puts "What is client name?"
+nameClient = gets.chomp
+
+puts "What is client age?"
+ageClient = gets.chomp.to_i 
+
+puts "How many rooms will be decorated?"
+roomsClient = gets.chomp.to_i
+
+puts "What is the client favorite color scheme?"
+colorClient = gets.chomp
+
+puts "What is client preferred decor theme?"
+decorClient = gets.chomp
+
+puts "Will client be financing payment? True or False"
+financeClient = gets.chomp.downcase
+if financeClient == "true"
+	financeClient = true
+else
+	financeClient = false
+end
+
+puts "Is this a returning client? True or False"
+returningClient = gets.chomp.downcase
+if returningClient == "true"
+	returningClient = true
+else
+	returningClient = false
+end
 
 # add responeses into empty hash
 
 # print hash
-
+p client_data
 # prompt for any updates to a key and convert to a symbol
 
 # prompt for updated value to the key
 
 # use responses to update the hash, if "none" is entered, do not change
-#   anything 
+	# anything 
 
 # print hash
 
