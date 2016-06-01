5da286e7bd41ff26856ebb170f23bd9e684736b4
Jason Pelletier <jpelletier19@me.com>
2016-06-01 15:54:23 -0400

    type out pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,21 @@
+# DESIGNER HASH
+
+# create empty hash
+
+# propmt user for name, age, number of rooms, size of family, 
+#  favorite color scheme, decor theme, will be financing?, return client?
+#  and convert data types when necessary
+
+# add responeses into empty hash
+
+# print hash
+
+# prompt for any updates to a key and convert to a symbol
+
+# prompt for updated value to the key
+
+# use responses to update the hash, if "none" is entered, do not change
+#   anything 
+
+# print hash
+
