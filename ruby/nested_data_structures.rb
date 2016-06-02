c1520fc2618d4c4ba15cf9ca6cec976ae10e058b
Jason Pelletier <jpelletier19@me.com>
2016-06-02 16:33:05 -0400

    write psuedocode

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,13 @@
+# create hash of 3on3 basketball teams, with team names as key, hash of names of players key, 
+	# and value of array of player attributes - with height, age, skill(A-D)
+
+
+# print to screen the attributes of a player from specific team
+
+# print to screen the list of players on a specific team
+
+# a new season has started so add 1 to everyone's age
+
+# a player has gotten better so improve the player's skill rating
+
+# players were traded so move one player to another team and visa versa
