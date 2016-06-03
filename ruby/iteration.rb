fec490ccd31857d09e7eeceb85c412c6cccca359
Jason Pelletier <jpelletier19@me.com>
2016-06-03 17:08:56 -0400

    copy code from pairing

---
+++ ruby/iteration.rb
@@ -0,0 +1,51 @@
+food = ["pizza", "Beer", "apple", "lobster", "stake"]
+states = {new_hampshire: "concord", california: "sacramento", texas: "austin", ohio: "columbus"}
+new_food = []
+
+#food.each do |items| 
+ #	puts "I do enjoy this #{items}"
+#end
+
+#food.map! do |items| 
+ #    "Ice Cream"
+#     new_food << "ice cream"
+#end
+
+#p food
+
+#states.each do |usa, rock|
+# 	puts "I have been there #{rock}, {usa}"
+# end
+
+# # Can't use a .map on a hash
+
+
+# food.map do |items| 
+#     if items.length < 5
+#     	food.delete(items)
+#     end
+# end
+# states.each do |state, capitol| 
+#     if state.length < 5
+#     	states.delete(state)
+#     end
+# end
+
+
+
+
+
+# food.map do |items| 
+#     food.delete_if {|items| items.length < 5}
+    
+# end
+# p food
+
+# states.each do |key, value|
+# 	states.delete_if {|key, value| key.length < 5}
+# end
+
+
+
+
+ 
