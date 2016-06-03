62664ed529b2b56c57a53e8c82c0a661818bf2eb
Jason Pelletier <jpelletier19@me.com>
2016-06-03 12:25:54 -0400

    complete next_consonant method with success tests

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -17,7 +17,7 @@ p switchName
 def next_vowel (char)
 	# name all vowels and turn into character arrays
 	allVowels = "aeiouAEIOU".chars
-  	# create index variables for each character passed through
+  	# create index variable for each character passed through
     vowelIndex = allVowels.index(char)	
     	# set IF statements for edge casses.  Else set character to 1
     		# added to vowel index 
@@ -31,10 +31,27 @@ def next_vowel (char)
 end
 
 ### tester for next_vowel
- p next_vowel ("U")
+ p next_vowel("U")
 
 ### change consonants to next letter in sequence and keep case specific
 
+def next_consonant (char)
+	# name all consonants and turn into character arrays
+	allConsonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".chars
+  	# create index variable for each character passed through
+    consonantIndex = allConsonants.index(char)	
+    	# set IF statements for edge casses.  Else set character to 1
+    		# added to consonant index 
+    	if char == "z"	
+    		char = "b"
+    	elsif char == "Z"	
+    		char = "B"
+    	else 
+    		char = allConsonants[consonantIndex+1]
+    	end
+end
 
+### tester for next_consonant
+p next_consonant("d")
 # incorporate all three methods into one method
 
