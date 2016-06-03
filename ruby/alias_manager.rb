fefd6d4421b94f787d936a384fd9c578779b8aa8
Jason Pelletier <jpelletier19@me.com>
2016-06-03 12:17:36 -0400

    complete next_vowel method with success tests

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,59 +1,39 @@
-# swap first and last name
+### swap first and last name
+
 def name_swap (firstName, lastName)
 	newName = lastName + " " + firstName
 end
-	#test for name_swap
-name = "Jason Pelletier"
+
+### tester for name_swap
+name = "Bob Vincent"
 splitName = name.split(' ')
 p splitName
 
 switchName = name_swap(splitName[0], splitName[1])
 p switchName
 
-# change vowels to next letter in sequence and keep case specific.  
+###change vowels to next letter in sequence and keep case specific.  
 
-#upperVowels = "AEIOU".chars
-
-#def vowel_index(char)
-    #allVowels = "aeiouAEIOU".chars
-    #allVowels.index(char)
-#end
-
-
-def next_vowel (changedName)
-	# turn vowels and name into character arrays
+def next_vowel (char)
+	# name all vowels and turn into character arrays
 	allVowels = "aeiouAEIOU".chars
-  	nameCharacters = changedName.chars
-  	# loop through each character in the name
-  	nameCharacters.each do |char|
-    	# create index variables for each character passed through
-    	charIndex = nameCharacters.index(char)
-    	vowelIndex = allVowels.index(char)	
-    	# set IF statements for edge casses.  Else set name character to 1
-    	# added to vowel index 
+  	# create index variables for each character passed through
+    vowelIndex = allVowels.index(char)	
+    	# set IF statements for edge casses.  Else set character to 1
+    		# added to vowel index 
     	if char == "u"	
-    		nameCharacters[charIndex] = "a"
+    		char = "a"
     	elsif char == "U"	
-    		nameCharacters[charIndex] = "A"
-    	else #allVowels.include?(char) == true
-    		
-    		p "letter in name: #{nameCharacters[charIndex]}"
-    		p "index is: #{charIndex}"  
-    		p "letter in vowels: #{allVowels[vowelIndex]}"
-    		p "index is: #{vowelIndex}"
-    		p "letter in vowels add 1: #{allVowels[vowelIndex+1]}"
-    		p "index is: #{vowelIndex+1}"
-    		nameCharacters[charIndex] = allVowels[(vowel_index+1)]
+    		char = "A"
+    	else 
+    		char = allVowels[vowelIndex+1]
     	end
-    end	
 end
 
-	#tester for next_vowel
- p next_vowel ("eauU")
- #p vowel_index("i")
+### tester for next_vowel
+ p next_vowel ("U")
 
-
-# change consonants to next letter in sequence and keep case specific
+### change consonants to next letter in sequence and keep case specific
 
 
 # incorporate all three methods into one method
