b5c1d0e5069a501f409c4ebe5ede4b450039fd14
Jason Pelletier <jpelletier19@me.com>
2016-06-03 11:08:28 -0400

    basic psuedocode and swap name method

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,19 @@
+# swap first and last name
+def name_swap (firstName, lastName)
+	newName = lastName + " " + firstName
+end
+
+name = "Jason Pelletier"
+splitName = name.split(' ')
+p splitName
+
+p name_swap(splitName[0], splitName[1])
+
+# change vowels to next letter in sequence and keep case specific.  
+
+
+# change consonants to next letter in sequence and keep case specific
+
+
+# incorporate all three methods into one method
+
