fed50228bfa538d8b9412aebad63add4267793e9
Jason Pelletier <jpelletier19@me.com>
2016-06-02 22:04:29 -0400

    complete teams and all players to league

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,13 +1,66 @@
-# create hash of 3on3 basketball teams, with team names as key, hash of names of players key, 
-	# and value of array of player attributes - with height, age, skill(A-D)
+# create hash of 3on3 basketball teams, with team names as key, value is hash of players with 
+	# key/values for name, age, and array of skills
 
+# THE 25-35 YR OLD 3 ON 3 BASKETBALL LEAGUE
+basketball_league = {
+	red_cows: {
+		team_name: 'Red Cows',
+		player1: {
+			name: "Bobby",
+		    age: 31,
+		    skills: ["rebounding", "shot blocking", "defense"] 
+		},
+		player2: {
+			name: "Sammy",
+		    age: 27,
+		    skills: ["dribbling", "passing"] 
+		},
+		player3: {
+			name: "Ben",
+		    age: 28,
+		    skills: ["shooting", "defense", "rebounding"] 
+		},
+	},
+	blue_moose: {
+		team_name: 'Blue Moose',
+		player1: {
+			name: "Larry",
+		    age: 33,
+		    skills: ["shooting", "passing", "defense"] 
+		},
+		player2: {
+			name: "Kevin",
+		    age: 32,
+		    skills: ["post ups", "shooting"] 
+		},
+		player3: {
+			name: "Robbie",
+		    age: 29,
+		    skills: ["rebounding", "shot blocking"] 
+		},
+	},
+	green_zebras: {
+		team_name: 'Green Zebras',
+		player1: {
+			name: "Mickey",
+		    age: 25,
+		    skills: ["dribbling", "shooting", "defense"] 
+		},
+		player2: {
+			name: "Charlie",
+		    age: 34,
+		    skills: ["defense"] 
+		},
+		player3: {
+			name: "David",
+		    age: 28,
+		    skills: ["dribbling", "defense", "shooting"] 
+		},
+	},
+}
 
-# print to screen the attributes of a player from specific team
+# print to screen the skills of a player from specific teams
 
 # print to screen the list of players on a specific team
 
-# a new season has started so add 1 to everyone's age
-
-# a player has gotten better so improve the player's skill rating
-
-# players were traded so move one player to another team and visa versa
+# a player has improved so add a new skill to their array
