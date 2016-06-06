48c3adbfefcdd506e03d31c51d75d68a8107ed2e
Jason Pelletier <jpelletier19@me.com>
2016-06-06 11:11:56 -0400

    puppy species name, characteristics, and behaviors

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# SPECIES ------------------------
+# Puppus Cuteacuss
+
+# CHARACTERISTICS ----------------
+# Name: varies
+# Color: varies
+# Eyes: large
+# Ears: fluffy
+# Hyper: yes
+# Friendly: yes
+
+# BEHAVIOR -----------------------
+# Jump
+# Yelp
+# Run
+# Snuggle
