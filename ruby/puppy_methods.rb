bc933cea2a948c27b9a596b41ab238daec64ffe1
Jason Pelletier <jpelletier19@me.com>
2016-06-06 21:08:54 -0400

    add new class with methods and iterations

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -27,9 +27,46 @@ class Puppy
 
 end
 
+class Hotel_guest
+
+	def initialize
+		puts "New Guest added"
+	end
+
+	def bill(days)
+		puts "Guest owes $#{days * 350}"
+	end
+
+	def wants_wakeup(time)
+		puts "Its #{time}.....WAKE UP!"
+	end
+end
+
+frank = Hotel_guest.new
+frank.bill(3)
+frank.wants_wakeup("7:30AM")
+
+guest_arr = []
+
+50.times do |x|
+	x = Hotel_guest.new 
+	guest_arr << x
+end
+
+p guest_arr
+
+guest_arr.each do |guest|
+	guest.bill(4)
+	guest.wants_wakeup("8:00am")
+end
+
+
+
+
 Gizmo = Puppy.new
 Gizmo.fetch("ball")
 Gizmo.speak(3)
 Gizmo.roll_over
 p Gizmo.dog_years(3)
 p Gizmo.high_five
+
