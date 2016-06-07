f31f582f7c6cce4c73b79ac44b533a74e0fbe8f9
Jason Pelletier <jpelletier19@me.com>
2016-06-06 20:18:40 -0400

    add methods to Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,26 @@ class Puppy
     toy
   end
 
+  def speak(count)
+  	count.times { |times| puts "Woof!"}
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(age)
+  	age * 7
+  end
+
+  def high_five
+  	5
+  end
 end
 
+Gizmo = Puppy.new
+Gizmo.fetch("ball")
+Gizmo.speak(3)
+Gizmo.roll_over
+p Gizmo.dog_years(3)
+p Gizmo.high_five
