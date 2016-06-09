57e80d83cd2b02466bc27ee29d6f838e219d49c9
Jason Pelletier <jpelletier19@me.com>
2016-06-08 23:25:38 -0400

    pseudocode jedi class

---
+++ ruby/solo.rb
@@ -0,0 +1,19 @@
+### JEDI CLASS ###
+
+# make a Jedi class with:
+
+# Characteristics
+
+# Name that can vary
+# Age that can vary
+# Species that can be a human, bothan, twi'lek, wookie, rodian, or nautolan
+# Home planet that can be tatooine, naboo, hoth, coruscant, bespin, endor, or alderaan
+# Light saber color that can be blue, green, orange, yellow, or purple
+
+# Abilities
+
+# Levitate an object
+# Force push an object
+# Mind trick with a saying
+# Force grab and object
+
