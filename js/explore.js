e3e2fe2d2126dedba3b82a3edf21ab31ba012b7a
Jason Pelletier <jpelletier19@me.com>
2016-06-14 15:47:26 -0400

    complete pseudocode

---
+++ js/explore.js
@@ -0,0 +1,13 @@
+// PSEUDOCODE
+// Make a word
+// Start at last letter and make it first
+// Go thru each letter following and do the same
+// Print out new reversed word
+
+
+// FUNCTION DEFINITION
+
+
+
+
+// DRIVER CODE
